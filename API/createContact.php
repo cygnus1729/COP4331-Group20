c6b5cb43482d0e0cf1e73fc483a7f60978c0c3d6
jeIIycodes <anthonycastillolmk@gmail.com>
2025-02-13 17:56:57 -0500

    Updates

---
+++ API/createContact.php
@@ -0,0 +1,58 @@
+<?php
+	$inData = getRequestInfo();
+
+    // Might have to replace these depending on name of fields in DB
+	$userId    = $inData["userId"];      
+	$firstName = $inData["firstName"];
+	$lastName  = $inData["lastName"];
+	$email     = $inData["email"];
+
+	// We need to replace with DB credentials and name to connect
+	$conn = new mysqli("localhost", "DB_USER", "DB_PASSWORD", "DB_NAME");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Contacts (UserID, FirstName, LastName, Email) VALUES (?,?,?,?)");
+		$stmt->bind_param("isss", $userId, $firstName, $lastName, $email);
+		$stmt->execute();
+
+		if ($stmt->affected_rows > 0)
+		{
+			$newId = $stmt->insert_id;
+			returnWithInfo($newId);
+		}
+		else
+		{
+			returnWithError("Error creating contact");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err)
+	{
+		$retValue = '{"id":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo($newId)
+	{
+		$retValue = '{"id":' . $newId . ',"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
---
+++ API/deleteContact.php
@@ -0,0 +1,54 @@
+<?php
+	$inData = getRequestInfo();
+    
+    // Might have to replace
+	$contactId = $inData["contactId"];
+
+	// More replacing
+	$conn = new mysqli("localhost", "DB_USER", "DB_PASSWORD", "DB_NAME");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
+		$stmt->bind_param("i", $contactId);
+		$stmt->execute();
+
+		if ($stmt->affected_rows > 0)
+		{
+			returnWithInfo("Contact deleted successfully");
+		}
+		else
+		{
+			returnWithError("No contact found or unable to delete");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo($msg)
+	{
+		$retValue = '{"error":"","message":"' . $msg . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
